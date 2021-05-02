4e36289e7d9c025ee75a4db6a613b44cc582cc80
Brad Cavanagh <brad.cavanagh@gmail.com>
2021-05-01 21:18:26 -0700

    remove debugging logs, test using AWS SES SDK for sending mail

--- LetterSender/ApproveLetter.cs
+++ LetterSender/ApproveLetter.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Amazon;
+using Amazon.SimpleEmailV2;
+using Amazon.SimpleEmailV2.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -23,11 +27,8 @@ namespace LetterSender
 			// and decode the remainder.
 			using var streamReader = new StreamReader(req.Body);
 			var requestBodyString = await streamReader.ReadToEndAsync();
-			log.LogInformation("Request body string: {BodyString}", requestBodyString);
 			var urlDecoded = WebUtility.UrlDecode(requestBodyString[8..]);
-			log.LogInformation("Decoded: {Decoded}", urlDecoded);
 			var submission = Utils.ExtractJsonPayload<SlackSubmission>(urlDecoded);
-			log.LogInformation("Body: {RequestBodyString}", requestBodyString);
 
 			var token = Utils.GetEnvironmentVariable("SLACK_TOKEN");
 			if (!token.Equals(submission.Token))
@@ -36,6 +37,30 @@ namespace LetterSender
 			}
 
 			log.LogInformation("{ResponseUrl}", submission.ResponseUrl);
+
+			var sender = "brad.cavanagh@gmail.com";
+			var receiver = "brad.cavanagh@gmail.com";
+			var subject = "test from azure function";
+			var body = "this is a test";
+
+			using var client = new AmazonSimpleEmailServiceV2Client(RegionEndpoint.CACentral1);
+			var sendRequest = new SendEmailRequest
+			{
+				FromEmailAddress = sender,
+				Destination = new Destination
+				{
+					ToAddresses = new List<string> { receiver }
+				},
+				Content = new EmailContent
+				{
+					Simple = new Message
+					{
+						Body = new Body { Text = new Content { Data = body } },
+						Subject = new Content { Data = subject },
+					}
+				}
+			};
+			var response = await client.SendEmailAsync(sendRequest);
 
 			return new HttpResponseMessage(HttpStatusCode.Accepted);
 		}
